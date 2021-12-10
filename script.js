4354ad1a12b403f35e32ce34a6d040095510523f
Ragil14 <rahil.gilani@gmail.com>
2021-12-10 01:26:37 -0600

    added generatePassword() & pseudocoded steps

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-// Assignment Code
+  // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
+  // generatePassword function()
+function generatePassword() {
+  console.log("BUTTON CLICKED")
+    // 1. prompt the user for pw criteria
+    //   1a. pw length between 8 & 128
+    //   1b. prompt user for character types (lowercase, uppercase, numeric &/or special characaters)
+    // 2. validate the input
+    // 3. generate pw based on criteria
+
+
+    // 4. display pw to the page 
+  return "generated pW will go here";
+}
+
+
+  // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +26,6 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+
+  // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
